74b311c596eb37026321d8c2eaff5d81b9b86909
Juan Gabriel Tavárez López <jtavarez@intellisys.com.do>
2022-03-17 14:04:23 -0400

    feat: home responsive

--- src/shared/components/Navbar/index.tsx
+++ src/shared/components/Navbar/index.tsx
@@ -9,6 +9,8 @@ export const Navbar = () => {
   return (
     <div>
       <nav className={`${styles.navbar} ${show && styles.active} container mx-auto`}>
+        <h3 className={styles.name}>Salvador Dalí</h3>
+
         <div className={styles.hamburger} onClick={toggleNavbar}>
           <div></div>
           <div></div>
@@ -19,11 +21,17 @@ export const Navbar = () => {
           {
             navlinks.map(({ to, text }) => (
               <li key={text} onClick={toggleNavbar}>
-                <NavLink to={to} className={({ isActive }) => isActive ? 'text-light-blue' : ''}>{text}</NavLink>
+                <NavLink to={to} className={({ isActive }) => isActive ? 'active-link' : ''}>{text}</NavLink>
               </li>
             ))
           }
         </ul>
+
+        <div className={styles.social}>
+          <a href='https://github.com/JuanGabriel2960/salvador-dali-museum' target="_blank" rel='noreferrer'>
+            <img src='./github.png' alt='' />
+          </a>
+        </div>
       </nav>
     </div>
   )
